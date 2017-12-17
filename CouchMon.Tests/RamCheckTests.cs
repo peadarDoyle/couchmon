e9142fdeb1cd4d529110338514f354a45d378c39
Peadar Doyle <peadar.doyle@neocles.io>
2017-12-17 16:49:53 +0100

    Added empty test

---
+++ CouchMon.Tests/RamCheckTests.cs
@@ -0,0 +1,20 @@
+﻿using Couchbase.Configuration.Server.Serialization;
+using Couchbase.Core;
+using Couchmon.Checks;
+using Couchmon.Couchbase;
+using Moq;
+using Nimator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Couchmon.Tests
+{
+    /// <summary>
+    /// Not implemented - see the DocumentCheckTests since the tests would be very similar
+    /// </summary>
+    public class RamCheckTests
+    {
+    }
+}
